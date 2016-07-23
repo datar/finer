a9fcaae051fcfcd395abb1d339f7c58ac6bb8e85
datar <tianxian@gmail.com>
2016-07-22 22:28:52 -0700

    get and normalize people info from page and file

--- parsor/people_china_future.py
+++ parsor/people_china_future.py
@@ -1,9 +1,32 @@
 import lxml.html
 
 
-def get_people(page):
+def get_people_from_page(page):
 	document = lxml.html.fromstring(page)
 	table = []
 	for row in document.xpath('//table/tr'):
 		table.append([td.text for td in row])
-	return tabl
+	if len(table) > 0:
+		return table[1:]
+	else:
+		return table
+
+
+def get_info_from_filename(filename):
+	date_str = filename[0:8]
+	company_code = filename[11:15]
+	return date_str, company_code
+
+
+def get_people_from_file(filename):
+	date_str, company_code = get_info_from_filename(filename)
+	with open(filename) as infile:
+		page = infile.read()
+		content = get_people_from_page(page)
+	infile.close()
+	return date_str, company_code, content
+
+
+def normalize_people_daily_info(date_str, company_code, page):
+	n = [x.extend([date_str, company_code]) for x in page]
+	return n
