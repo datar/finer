248285ec30e635ed776eb7eb63535bcb0c6e3a4a
datar <tianxian@gmail.com>
2018-06-03 00:51:08 -0700

    restart

---
+++ feed/china_market.py
@@ -0,0 +1,2 @@
+company_people_page_url_pattern = 'http://www.cfachina.org/cfainfo/organbaseinfoOneServlet?organid=+%s+&currentPage=1&pageSize=2000&selectType=personinfo&all=undefined'
+ALL_PEOPLE_PAGE_URL = 'http://www.cfachina.org/cfainfo/personinfoServlet?organid=&currentPage=1&pageSize=100000&selectType=check&cardid='
--- finer.py
+++ finer.py
@@ -7,14 +7,14 @@ import os
 import requests
 import lxml.html
 
+import feed.china_market
 
 __author__ = 'gnix'
-company_people_page_url_pattern = 'http://www.cfachina.org/cfainfo/organbaseinfoOneServlet?organid=+%s+&currentPage=1&pageSize=2000&selectType=personinfo&all=undefined'
-ALL_PEOPLE_PAGE_URL = 'http://www.cfachina.org/cfainfo/personinfoServlet?organid=&currentPage=1&pageSize=100000&selectType=check&cardid='
 try_time = 10
 
-RAW_DATA_DIR = '/home/ec2-user/finer/RawData'
-TEST_DATA_DIR = '/home/ec2-user/finer/TestData'
+RAW_DATA_DIR = '/Users/gnix/Code/finer/RawData'
+TEST_DATA_DIR = '/Users/gnix/Code/finer/TestData'
+DEFAULT_HEADERS = {'User-Agent': "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
 
 
 def get_company_codes():
@@ -27,13 +27,14 @@ def get_company_codes():
 
 
 def get_company_people(company_code):
-    page_url = company_people_page_url_pattern % (company_code, )
+    page_url = feed.china_market.company_people_page_url_pattern % (company_code, )
     print('START::' + company_code)
     print('Fetching::' + page_url)
     page_content = ''
     for i in range(try_time):
         try:
-            page_content = urllib.request.urlopen(page_url, timeout=10).read()
+            req = urllib.request.Request(page_url, headers=DEFAULT_HEADERS)
+            page_content = urllib.request.urlopen(req, timeout=10).read()
             if len(page_content) < 1000:
                 print('::'.join([company_code, 'try no.', str(i)]))
                 time.sleep(6)
@@ -71,7 +72,9 @@ def save_all_people_in_one_page():
     filename = os.path.join(TEST_DATA_DIR, basename)
     if os.path.isfile(filename) and os.path.getsize(filename) > 1000:
         return 0
-    content = urllib.request.urlopen(ALL_PEOPLE_PAGE_URL, timeout=10).read()
+    page_url = feed.china_market.ALL_PEOPLE_PAGE_URL
+    req = urllib.request.Request(page_url, headers=DEFAULT_HEADERS)
+    content = urllib.request.urlopen(req, timeout=10).read()
     out_file = open(filename, 'wb')
     out_file.write(content)
     out_file.close()
