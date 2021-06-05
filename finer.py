0edc1206215fea319abd858d0d226340bec00c77
datar <tianxian@gmail.com>
2021-06-05 09:29:07 -0700

    new API

--- finer.py
+++ finer.py
@@ -1,20 +1,22 @@
-#import Parsor.Futures
 import urllib.request
 import urllib.error
 import datetime
 import time
 import os
+import json
 import requests
 import lxml.html
 
 import feed.china_market
+#import Parsor.Futures
 
 __author__ = 'gnix'
 try_time = 10
 
 RAW_DATA_DIR = '/Users/gnix/Code/finer/RawData'
 TEST_DATA_DIR = '/Users/gnix/Code/finer/TestData'
-DEFAULT_HEADERS = {'User-Agent': "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
+JSON_DATA_DIR = '/Users/gnix/Code/finer/JSONData'
+DEFAULT_HEADERS = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:83.0) Gecko/20100101 Firefox/83.0"}
 
 
 def get_company_codes():
@@ -74,11 +76,60 @@ def save_all_people_in_one_page():
         return 0
     page_url = feed.china_market.ALL_PEOPLE_PAGE_URL
     req = urllib.request.Request(page_url, headers=DEFAULT_HEADERS)
-    content = urllib.request.urlopen(req, timeout=10).read()
+    content = urllib.request.urlopen(req, timeout=30).read()
     out_file = open(filename, 'wb')
     out_file.write(content)
     out_file.close()
     return 1
+
+
+def get_trunk_of_employee(current_page, page_size):
+    request_url = feed.china_market.trunk_of_all_employee % (current_page, page_size)
+    print(request_url, "\n")
+    req = urllib.request.Request(request_url, headers=DEFAULT_HEADERS)
+    page_content = urllib.request.urlopen(req, timeout=30).read()
+    return page_content
+
+
+def save_all_employee(date_str):
+    output_file_pattern = "%s_all_employee_%06d_%06d.json"
+    current_page = 1
+    page_size = 500
+    page_start = (current_page-1)*page_size+1
+    while True:
+        page_content = get_trunk_of_employee(current_page, page_size)
+        result = json.loads(page_content.decode("utf-8"))
+        if 'errcode' not in result:
+            print(page_content)
+            print('error_code missing\n')
+            return
+
+        if 'msg' not in result:
+            print('msg is missing in result\n')
+            return
+
+        if 'data' not in result:
+            print('data is missing in result\n')
+            return
+
+        data = result['data']
+        if 'dataList' not in data:
+            print('dataList is missing in result data')
+
+        employee = data['dataList']
+        total = data['total']
+        result_size = len(employee)
+        page_end = page_start + result_size - 1
+        filename = output_file_pattern % (date_str, page_start, page_end)
+        filepath = os.path.join(JSON_DATA_DIR, filename)
+        out_file = open(filepath, 'wb')
+        out_file.write(page_content)
+        out_file.close()
+        print("Saved: %s, %d-%d of %d\n" % (filename, page_start, page_end, total))
+        if result_size < page_size:
+            break
+        page_start = page_start + result_size
+        current_page += 1
 
 
 def save_all_data():
@@ -95,7 +146,8 @@ def save_all_data():
 
 
 def main():
-    save_all_data()
+    #save_all_data()
+    save_all_employee('20210604')
 
 
 if __name__ == '__main__':
