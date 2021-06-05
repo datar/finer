204a806dd8c81759dedddc141eaea0a59bfe573f
datar <tianxian@gmail.com>
2021-06-05 09:30:15 -0700

    new pattern for china market

--- feed/china_market.py
+++ feed/china_market.py
@@ -1,2 +1,3 @@
 company_people_page_url_pattern = 'http://www.cfachina.org/cfainfo/organbaseinfoOneServlet?organid=+%s+&currentPage=1&pageSize=2000&selectType=personinfo&all=undefined'
 ALL_PEOPLE_PAGE_URL = 'http://www.cfachina.org/cfainfo/personinfoServlet?organid=&currentPage=1&pageSize=100000&selectType=check&cardid='
+trunk_of_all_employee = 'http://www.cfachina.org/orc-report/api/practitioners/baseInfo?pageNo=%d&pageSize=%d&flag=&keyword='
